a917370500a69a6af5568cab0177b371d1ea28ca
Lavell Wesley <lavellthedev+github@gmail.com>
2020-03-16 22:18:01 -0400

    started filling out backend

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+
+require('dotenv').config();
+
+//How we create our express server
+const app = express();
+const port = process.env.PORT || 9000;
+
+//middleware/parse json
+app.use(cors());
+app.use(express.json());
+
+//what starts the server
+app.listen(port, () =>{
+  console.log(`Server is running on over ${port}`);
+});
