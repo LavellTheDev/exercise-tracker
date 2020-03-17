b8204cd9e1fc824166a8ae5d5e7ba27295f3a628
Lavell Wesley <lavellthedev+github@gmail.com>
2020-03-17 10:04:40 -0400

    fixed problem with wrong MongoDB info, currently building out backend

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,27 @@
+
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-//How we create our express server
 const app = express();
 const port = process.env.PORT || 9000;
 
-//middleware/parse json
 app.use(cors());
 app.use(express.json());
 
-//database uri we get from dashboard
 const uri = process.env.ATLAS_URI;
-//pass in uri here v
-mongoose.connect(uri, {
-useUnifiedTopology: true,
-useNewUrlParser: true,
-useCreateIndex: true,
-});
-
-
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true }
+);
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
-//what starts the server
-app.listen(port, () =>{
-  console.log(`Server is running on over ${port}`);
-});
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.listen(port, () => {
+  console.log(`Server is over ${port}!!!`);
+})
